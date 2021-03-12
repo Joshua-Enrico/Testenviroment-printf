d83734b14f18f3e4bf59706f1ecdaa46915f7d64
renatoleonholberton <2829@holbertonschool.com>
2021-03-12 23:50:04 +0000

    Add _printf prtototype

--- holberton.h
+++ holberton.h
@@ -9,6 +9,7 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 void new_function();
+int _printf(const char *format, ...);
 
 /**
  * struct op_o - Struct fly_ops
