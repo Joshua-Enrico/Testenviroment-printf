1e6e2b3208e83364e84980043f4ad9f742ff9691
yoyogold-a11 <joshuaenrico123@gmail.com>
2021-03-13 02:30:12 +0000

    estructurando las funciones

--- funciones.c
+++ funciones.c
@@ -1,3 +1,33 @@
 
 #include "holberton.h"
 #include <stdio.h>
+
+
+int op_c(va_list op_d)/*char*/
+{
+
+
+
+}
+
+
+int op_ss(va_list op_d)
+{
+
+
+}
+int op_i(va_list op_d)/*entero*/
+{
+
+
+}
+int op_dd(va_list op_d)
+{
+
+
+}
+int op_z(va_list op_d)
+{
+
+
+}
