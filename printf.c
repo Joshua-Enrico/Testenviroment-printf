13c25d78c9cd410e425e34ca1fe2e5e2e7c22de3
yoyogold-a11 <joshuaenrico123@gmail.com>
2021-03-13 02:28:14 +0000

    estructurando el codigo printf

--- printf.c
+++ printf.c
@@ -1,44 +1,24 @@
-
 #include "holberton.h"
-
-
+#include <stdio.h>
 
 
 int _printf(const char *format, ...)
 {
+    int i, j;
+    int count = 0;
+    va_list list;
     op_t op_s[] = {
         {"c", op_c},
-        {"s", op_i},
-        {"i", op_f},
-        {"d", op_z},
-        {"d", op_z},
+        {"s", op_ss},
+        {"i", op_i},
+        {"d", op_dd},
+        {'%', op_z},
         {NULL, NULL},
 
     };
 
-    unsigned int i = 0, j = 0;
-    char *x = "";
-    va_list opp;
-    va_start(opp, format);
+va_start(list, format);
 
-    while (format != NULL && format[i] != '\0')
-    {
-        j = 0;
-        while (op_s[j].op != NULL)
-        {
-            if (format[i] == op_s[j].op[0])
-            {
-                printf("%s", x);
-                op_s[j].stargate(opp);
-                x = ", ";
-            }
-            j++;
-        }
-        i++;
-    }
-    printf("\n");
-    va_end(opp);
-}
 
 
 
