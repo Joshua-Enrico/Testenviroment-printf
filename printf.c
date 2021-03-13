e556244a387aad90196ef131f5a36752d5355b9c
yoyogold-a11 <joshuaenrico123@gmail.com>
2021-03-13 08:50:26 +0000

    aun armando la funcion

--- printf.c
+++ printf.c
@@ -1,24 +1,36 @@
 #include "holberton.h"
-#include <stdio.h>
-
 
 int _printf(const char *format, ...)
 {
-    int i, j;
-    int count = 0;
-    va_list list;
-    op_t op_s[] = {
+
+    op_t ops[] = {
         {"c", op_c},
-        {"s", op_ss},
-        {"i", op_i},
-        {"d", op_dd},
-        {'%', op_z},
+        {"s", op_s},
+        {"i", op_d},
+        {"d", op_d},
+        {"%", op_mod},
         {NULL, NULL},
 
     };
+    va_list list;
+    int contador = 0;
+    int i = 0, j = 0;
 
-va_start(list, format);
+    va_start(list, format);
 
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            for (j = 0; ops[j].op != NULL; j++)
+            {
+                if (format[i] == ops[j].op[0])
+                {
+                    contador += ops[j].f(list);
 
-
-
+                    /* aun me falta ver varias cosas aqui, asi que solo me quedo en la parte que comparo */
+                }
+            }
+        }
+    }
+}
