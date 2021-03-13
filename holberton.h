8eb90fa426983285d9256b2e575fb228942574b8
yoyogold-a11 <joshuaenrico123@gmail.com>
2021-03-13 08:17:40 +0000

    modifique para que tenga solo las funciones que necesita

--- holberton.h
+++ holberton.h
@@ -1,33 +1,25 @@
 #ifndef HOL_H
 #define HOL_H
+#include <stdio.h>
 #include <stdarg.h>
-
-int _putchar(char c);
-int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);	
-
-void new_function();
-int _printf(const char *format, ...);
+#include <stdlib.h>
 
 /**
- * struct op_o - Struct fly_ops
- * Desc: Flying operators
+ * struct op - Struct op
+ *
  * @op: The operator
- * @stargate: The function associated
+ * @f: The function associated
  */
-typedef struct op_o
+typedef struct op
 {
 	char *op;
-	void (*stargate)(va_list valist);
+	int (*f)(va_list);
 } op_t;
 
-int op_c(va_list op_d);/*char*/
-int op_ss(va_list op_d);/*string*/
-int op_i(va_list op_d);/*entero*/
-int op_dd(va_list op_d);/*number of integer inter*/
-int op_z(va_list op_d);/* % */
-#endif /* HOL_H*/
+int _putchar(char a);
+int _printf(const char *format, ...);
+int op_c(va_list op_l);
+int op_s(va_list op_l);
+int op_mod(va_list op_l);
+int op_d(va_list op_l);
+#endif /* HOL_H */
